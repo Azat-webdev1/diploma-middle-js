2565799e8cacabdcd00382c794db5a511f40021f
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-09-13 13:10:55 +0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `bundle.${ext}` : `bundle.${ext}`;
+
+module.exports = {
+  //context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry:'./src/index.js',
+  output: {
+    filename: `./${filename('js')}`,
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8000,
+    
+  },
+  devtool: isProd ? false : 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+],
+};
